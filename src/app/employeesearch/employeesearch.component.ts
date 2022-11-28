0a4532752f1650532acc4f414f7b34a9769bea32
anugraha4 <96745531+anugraha4@users.noreply.github.com>
2022-11-29 02:11:13 +0530

    employee search added

---
+++ src/app/employeesearch/employeesearch.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-employeesearch',
+  templateUrl: './employeesearch.component.html',
+  styleUrls: ['./employeesearch.component.css']
+})
+export class EmployeesearchComponent {
+
+}
